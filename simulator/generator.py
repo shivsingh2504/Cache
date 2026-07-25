cae71016fcf23d587875e2d3d691574065c71ded
shivsingh2504 <shivsinghs2504@gmail.com>
2026-07-26 02:32:11 +0530

    Added the generate function in generator.py

--- simulator/generator.py
+++ simulator/generator.py
@@ -21,4 +21,19 @@ def __init__(self,config:WorkloadConfig)->None:
   self._burst_key : int = 0
   self._burst_enabled : bool = config.burst_prob > 0.0
   
+def generate(self) -> Iterator[AccessEvent]:
+  total_requests = config._num_request
+  produced = 0
+  remaining = 0
+  while produced < total_requests:
+    remaining = total_requests - produced
+    chunk_size = self._next_chunk_size(remaining)
+    key_chunk = self._sample_base_keys(chunk_size)
+    if self._bursts_enabled:
+      self._inject_bursts(key_chunk)
+    for key in key_chunk:
+      yield AccessEvent(timestamp = timestamp , key = int(key))
+      timestamp+=1
+    produced += chunk_size
+    self._advance_drift_state(chunk_size)
     
