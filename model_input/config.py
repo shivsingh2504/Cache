ed895d83fbdfbe48ce6dd9c68006e7a07189bf1c
shivsingh2504 <shivsinghs2504@gmail.com>
2026-08-01 18:00:01 +0530

    Created a class defining the parameter and window size for the model input in config.py

---
+++ model_input/config.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+from data import dataclass
+
+@dataclass(frozen=True,slots=True)
+
+class  SequenceConfig:
+  window_size : int = 50
+  
+  def __post_init__(self)->None:
+    self._validate_window_size()
+    
+  def _validate_window_size(self)->None:
+    if self.window_size <= 0:
+      raise ValueError(f"Window size must be a positive integer")
