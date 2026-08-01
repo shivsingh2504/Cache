cd5b8c32ffe7b008f05a87a596664dec8f6a86b5
shivsingh2504 <shivsinghs2504@gmail.com>
2026-08-02 02:29:53 +0530

    Added the SequenceBuilder to the builder.py which deals with the deque logic according to the window size

--- model_input/builder.py
+++ model_input/builder.py
@@ -1,6 +1,20 @@
 from __future__ import annotations
-from collections import abc
+from collections.abc import Iterator
 from feature.schema import FeatureVector
-
+from collections import deque
 from model_input.config import SequenceConfig
 
+class SequenceBuilder:
+  
+  def __init__(self,config:SequenceConfig)->None:
+    
+    self._config = config
+    
+  def build(self,feature_stream: Iterator[FeatureVector])-> Iterator[tuple[FeatureVector]]:
+    window_size = self._config.window_size
+    buffer : deque[FeatureVector] = deque(maxlen=window_size)
+    
+    for feature_vector in feature_stream:
+      buffer.append(feature_vector)
+      if len(buffer)==window_size:
+        yield tuple(buffer)
