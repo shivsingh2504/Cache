13382fc24d231bdd6f7dda6d402a042090a7eb0e
shivsingh2504 <shivsinghs2504@gmail.com>
2026-08-02 02:47:47 +0530

    Converted the feature vectors into tuples which are stored in a list format in tensorizer.py

--- model_input/tensorizer.py
+++ model_input/tensorizer.py
@@ -4,3 +4,9 @@ import dataclasses
 import numpy as np
 from feature.schema import FeatureVector
 
+class Tensorizer:
+  def tensorize(self,sequence: tuple[FeatureVector])->np.ndarray:
+    if not sequence:
+      raise ValueError("Cannot tensorize an empty sequence")
+    rows = [dataclasses.astuple(feature_vector) for feature_vector in sequence]
+    return np.ndarray(rows, dtype=np.float32)
