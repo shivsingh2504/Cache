1db547f3221b6c94e429f126d508f8a6ca5c72d9
shivsingh2504 <shivsinghs2504@gmail.com>
2026-08-05 16:52:55 +0530

    Defined the Q-network and forward function in network.py

--- RL/network.py
+++ RL/network.py
@@ -8,7 +8,19 @@ class QNetwork(nn.Module):
     
     super().__init__()
     self.config = config
-    self.layers : nn.Module = nn.Identity()
+    input_dim = config.cache_capacity * config.num_state_features
+    output_dim = config.cache_capacity
+    layer_size = [input_dim,*config.hidden_sizes,output_dim]
+    layers : list[nn.Module] = []
+    for i in range(len(layer_size)-1):
+      layers.append(nn.Linear(layer_size[i],layer_size[i+1]))
+      is_last_layer = i ==len(layer_size)-2
+      if not is_last_layer:
+        layers.append(nn.ReLU())
+    self.layers : nn.Module = nn.Sequential(*layers)
+    
     
   def forward(self,state:torch.Tensor)->torch.Tensor:
-    raise NotImplementedError
+    batch_size = state.shape[0]
+    flattened = state.reshape(batch_size,-1)
+    return self.layers(flattened)
